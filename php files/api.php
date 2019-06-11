686e181269910d4bc7ad3c017d939102a9da6829
JoyChenCN <354215459@qq.com>
2019-06-12 00:16:01 +0800

    Update api.php

--- php files/api.php
+++ php files/api.php
@@ -1,8 +1,9 @@
 <?php
-    $username = "yourusername";
-    $pass = "password";
-    $host = "yourhost";
-    $dbname = "yourdb";
+    header("Content-Type: text/html;charset=utf-8");
+    $username = "user";
+    $pass = "pass";
+    $host = "host";
+    $dbname = "db_name";
     $date = array();
     // 创建连接
     $conn = new mysqli($host, $username, $pass, $dbname);
@@ -10,35 +11,41 @@
     if ($conn->connect_error) {
         die("连接失败: " . $conn->connect_error);
     }
-    $conn->query("set names gbk");
+    $conn->query("set names utf8");
+    $sql = "SELECT * FROM jd_comment";
+    // set check box
 
-    $g_id = "g_id";
-    $g_nickname = "g_nickname";
-    $g_level = "g_level";
-    $sql = "SELECT ".$g_id.", ".$g_nickname." ,".$g_level." FROM jd_comment";
+    $g_nickname = true;
+    $g_level = true;
+    $g_id = false;
+    $g_num = true;
+    $g_score = true;
+    $g_content = false;
+    $g_client = true;
 
     $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
         $status = true;
         $msg = "成功";
         $i = 0;
         while($row = mysqli_fetch_array($result)){
-
-            $data [$i]['ID'] = $row['g_id'];
-            $data [$i]['Name'] = $row['g_nickname'];
-            $data [$i]['level'] = $row['g_level'];
+            $data [$i]['num'] = $g_num == false ? null : $row['num'];
+            $data [$i]['id'] = $g_id == false ? null : $row['g_id'];
+            $data [$i]['name'] = $g_nickname == false ? null : $row['g_nickname'];
+            $data [$i]['level'] = $g_level == false ? null : $row['g_level'];
+            $data [$i]['client'] = $g_id == false ? null : $row['g_client'];
+            $data [$i]['content'] = $g_content == false ? null : $row['g_content'];
             $i++;
         }
     }else {
         $status = false;
-        $msg = "数据查询失败";
+        $msg = "msg error";
         $valuse = array(
             $status,
             $msg
         );
     }
-    echo json($status,$msg,$data);
+    echo json($status, $msg, $data);
         //封装json 格式
     function json($status, $message = '', $data = array()) {
         if (!is_bool( $status )) {
@@ -49,8 +56,8 @@
             'message' =>$message,
             'data' => $data
         );
-//        echo json_encode ( $result,JSON_UNESCAPED_UNICODE);
-        print_r($result);
+        echo json_encode ( $result,JSON_UNESCAPED_UNICODE);
+//        print_r($result);
     }
     $conn->close();
 
